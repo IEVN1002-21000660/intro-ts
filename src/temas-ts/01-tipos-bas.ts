04bac30837ae8231806973c0dfe45a51ba6f7c8e
21000660 <21000660@alumnos.utleon.edu.mx>
2024-09-07 10:04:52 -0600

    Primer commit de intro-ts

---
+++ src/temas-ts/01-tipos-bas.ts
@@ -0,0 +1,18 @@
+var num1 = 23;
+let num2 = 12;
+ 
+const num3 = 34
+ 
+const sum = num1 + num2 + num3;
+ 
+console.log(sum)
+console.log(`the sum is: ${sum}`);
+ 
+let nombre:string = 'juan';
+let edad:number = 23;
+let activo:boolean = true;
+ 
+let matricula:number|string;
+ 
+matricula = 21000660;
+matricula = '21000660';
